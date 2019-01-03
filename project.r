ab7a1782eac2110fcf896ab9dd19ae238ab9a205
kevinc6 <pufferclown@gmail.com>
2019-01-03 15:07:43 +0800

    Plot1

--- project.r
+++ project.r
@@ -1,2 +1,22 @@
-print("hi")
-print("world!")
+library(ggplot2)
+library(tidyverse)
+library(corrplot)
+train <- read.csv ("train.csv", header=TRUE, sep=",")
+test <- read.csv("test.csv", header=TRUE, sep="," )
+all<- rbind(within(train, rm("SalePrice")), test)
+summary(train$SalePrice)
+ggplot(data=train, aes(x=SalePrice/100)) + geom_histogram (binwidth = 20 )
+
+num_vars <- train %>% select_if(is.numeric) %>% names() 
+
+# how many numerical variables?
+
+df_num <- train[ ,num_vars]
+cor_num <- cor(df_num, use="pairwise.complete.obs") 
+cor_sorted <- as.matrix(sort(cor_num[,'SalePrice'], decreasing = TRUE))
+cor_high <- names(which(apply(cor_sorted, 1, function(x) abs(x)>0.2)))
+cor_num <- cor_num[cor_high, cor_high]
+cex.before <- par("cex")
+par(cex = 0.5)
+corrplot.mixed(cor_num, diag = "l", tl.col="black", tl.pos = "lt")
+par(cex = cex.before)
