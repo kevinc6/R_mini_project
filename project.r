cf2ed133f53cdb95f75099a68fad20a4189e7b87
kevinc6 <pufferclown@gmail.com>
2019-01-03 23:56:31 +0800

    test

--- project.r
+++ project.r
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(ggplot2)
 library(tidyverse)
 library(corrplot)
@@ -8,12 +7,12 @@ all<- rbind(within(train, rm("SalePrice")), test)
 summary(train$SalePrice)
 ggplot(data=train, aes(x=SalePrice/100)) + geom_histogram (binwidth = 20 )
 
-num_vars <- train %>% select_if(is.numeric) %>% names() 
+num_vars <- train %>% select_if(is.numeric) %>% names()
 
 # how many numerical variables?
 
 df_num <- train[ ,num_vars]
-cor_num <- cor(df_num, use="pairwise.complete.obs") 
+cor_num <- cor(df_num, use="pairwise.complete.obs")
 cor_sorted <- as.matrix(sort(cor_num[,'SalePrice'], decreasing = TRUE))
 cor_high <- names(which(apply(cor_sorted, 1, function(x) abs(x)>0.2)))
 cor_num <- cor_num[cor_high, cor_high]
@@ -21,7 +20,5 @@ cex.before <- par("cex")
 par(cex = 0.5)
 corrplot.mixed(cor_num, diag = "l", tl.col="black", tl.pos = "lt")
 par(cex = cex.before)
-=======
-library(tidyverse)
 
->>>>>>> 3aeb91231ce52dd608aea79cc20c8dd58c01648e
+train$LotShape[is.na(train$LotShape)]
